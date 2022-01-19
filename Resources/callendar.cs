f352a2ed6673cf2ed679ce864d10cb5c79b2d53e
Daniil Shneyder <sarutobi.kon@yandex.ru>
2022-01-19 18:43:31 +0300

    добавил форму календаря

---
+++ Resources/callendar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace registration.Resources
+{
+    public partial class callendar : Form
+    {
+        public callendar()
+        {
+            InitializeComponent();
+        }
+
+        private void callendar_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
